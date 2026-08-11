ced40ba18e8035009f9f831d90b80d4ed4cd06bb
Aditya Shukla <adityaji476@gmail.com>
2026-08-11 23:29:35 +0530

    Pascal through Python

---
+++ Pascal.py
@@ -0,0 +1,9 @@
+# Pascal Triangle is a geometric arrangement of numbers in a triangle Shaped pattern.
+def pascal_triangle(n):
+    trow=[1]
+    y=[0]
+    for x in range(max(n,0)):
+        print(trow)
+        trow=[l+r for l,r in zip(trow+y,y+trow)]
+    return n>1
+pascal_triangle(8)
